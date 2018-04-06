e167489149cbc97376a32020ecabaee42ad1793b
Hiroki.T <hirodora@me.com>
2018-04-06 13:10:41 +0900

    fix: bugfix

--- C/1-1.c
+++ C/1-1.c
@@ -4,14 +4,15 @@
 int main() {
     int a[20];
     int i;
-    int sum;
+    int sum = 0;
     srand(time(NULL));
     for(i=0; i<20; i++){
         a[i]=rand()%6+1;
+        printf("%d\n", a[i]);
     }
     
     for(i=0;i<20;i++){
         sum += a[i];
     }
-    printf("%d\n", sum/20);
+    printf("AVERAGE: %d\n", sum/20);
 }
