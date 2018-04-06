1962ccdd573b1cca739ee9f24e650e04913cfbfe
Hiroki.T <hirodora@me.com>
2018-04-06 13:38:45 +0900

    change: 2次元配列になっているかチェックを追加

--- C/1-3a2.c
+++ C/1-3a2.c
@@ -2,7 +2,7 @@
 int main(int argc, char *argv[]) {
     printf("argc:\t%d\n", argc);
     for(int i;i<argc;i++){
-        printf("argv[%d]:\t%s\n", i, argv[i]);
+        printf("argv[%d]:\t%c\n", i, argv[i][0]);
     }
 }
 //レポートを書くときに、２次元配列になっているかチェックするためのファイル。適当にイジって遊べばOK
